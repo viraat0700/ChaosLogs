be939f58e76e7d5e4753bd19f4c4fbaafe68be98
Lalit Mohan <lalitmohan@Hawks-iMac.local>
2025-12-24 15:47:11 +0530

    fixed index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.get('/api/health', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
